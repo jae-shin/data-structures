06a8ac2f67c6820023bea5004126d3124dab6fa5
jae-shin <jae5464@gmail.com>
2016-06-08 15:56:00 -0700

    Implement size method for queue functional-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var size = 0;
+  var obj = {};
+
+  _.extend(obj, queueMethods);
+  console.log(typeof obj.size);
+  console.log(obj.size());
+
+  return obj;
 };
 
-var queueMethods = {};
+
+var queueMethods = {
+  size: function() {
+    return 0;
+  }
+};
 
 
