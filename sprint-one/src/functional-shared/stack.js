5a578362a8ef8705b77f519e11597ede2cadc275
jae-shin <jae5464@gmail.com>
2016-06-08 16:21:43 -0700

    Pass all specs for Stack functional-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.index = 0;
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.index;
+  },
+  push: function(val) {
+    this.index++;
+    this.storage[this.index] = val;
+  },
+  pop: function() {
+    if (this.index === 0) { return; }
+    var popped = this.storage[this.index];
+    delete this.storage[this.index];
+    this.index--;
+    return popped;
+  }
 
-
+};
