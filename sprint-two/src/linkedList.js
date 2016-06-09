9ae35b07811b3a989b66fa424a94a87238c59677
jae-shin <jae5464@gmail.com>
2016-06-09 10:06:26 -0700

    Pass specs for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.tail) { 
+      list.tail.next = node;
+    }
+    list.tail = node;
+    
+    if (list.head === null) {
+      list.head = node;
+    }
   };
 
   list.removeHead = function() {
+    var head = list.head;
+    if (head) {
+      list.head = head.next;
+      return head.value;
+    }
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;  
+    }
+    return false;
   };
 
   return list;
