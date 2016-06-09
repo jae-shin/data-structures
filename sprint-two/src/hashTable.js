eb4ab9f252e2b77b43f429615055bd1b78f3a9a0
jae-shin <jae5464@gmail.com>
2016-06-09 16:18:31 -0700

    Pass most Hash Table tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,21 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // if (this._storage.get(index)) {
+  //   index += 1;
+  // }
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  //var index = getIndexBelowMaxForKey(k, this._limit);
+  //this._storage.set(index, undefined);
+  this.insert(k, undefined);
 };
 
 
