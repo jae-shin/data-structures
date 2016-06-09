f68831453b582f45e5c7fae9f501f78f82dd448f
jae-shin <jae5464@gmail.com>
2016-06-08 19:37:04 -0700

    Pass all specs for Stack prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.storage = {};
+  stack.index = 0;
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.index;
+  }, 
+  push: function(val) {
+    this.index++;
+    this.storage[this.index] = val;
+  },
+  pop: function() {
+    if (this.index > 0 ) {
+      var popped = this.storage[this.index];
+      delete this.storage[this.index];
+      this.index--;
+      return popped;
+    }
+  }
+};
 
 
