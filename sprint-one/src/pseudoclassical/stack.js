d3489f0c4e663f3d004917d796fd434e4c13f9ea
jae-shin <jae5464@gmail.com>
2016-06-08 19:55:15 -0700

    Pass all specs for Stack in pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.index = 0;
 };
 
+Stack.prototype.size = function() {
+  return this.index;
+};
 
+Stack.prototype.push = function (value) {
+  this.index++;
+  this.storage[this.index] = value;
+};
+
+Stack.prototype.pop = function () {
+  if (this.index === 0) { return; }
+  
+  var popped = this.storage[this.index];
+  delete this.storage[this.index];
+  this.index--;
+  return popped;
+};
