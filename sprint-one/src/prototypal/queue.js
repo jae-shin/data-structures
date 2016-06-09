0b8c1a48ac4340d073196f7ae024704126f21af8
jae-shin <jae5464@gmail.com>
2016-06-08 19:44:03 -0700

    Pass all specs Queue prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.storage = {};
+  queue.start = 1;
+  queue.end = 1;
+  return queue;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  size: function() {
+    return this.end - this.start;
+  },
+  enqueue: function(value) {
+    this.storage[this.end] = value;
+    this.end++;
+  },
+  dequeue: function() {
+    if (this.start === this.end) { return; }
+    
+    var popped = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return popped;
+  }
+};
