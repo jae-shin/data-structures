1acbff02e386de8713e1ffe3b1a7a3932669efa0
jae-shin <jae5464@gmail.com>
2016-06-10 11:28:27 -0700

    Refactor Hash Table with buckets as arrays
    Includes patch for retreiving keys not present

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,24 +7,61 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var obj = this._storage.get(index);
-  if (obj === undefined) {
-    obj = {};
+  var bucket = this._storage.get(index);
+  if (!bucket) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    // check if a tuple with same key exists, if so overwrite
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === k) {
+        tuple[1] = v;
+        return;
+      }
+    }
+
+    // TODO: refactor for loop with native findIndex()
+
+    bucket.push([k, v]);
   }
-  obj[k] = v;
-  this._storage.set(index, obj);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index)[k];
+  // return this._storage.get(index)[k];
+  var bucket = this._storage.get(index);
+
+  if (!bucket) {
+    return;
+  }
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   //this._storage.set(index, undefined);
   // this.insert(k, undefined);
-  delete this._storage.get(index)[k];
+  //delete this._storage.get(index)[k];
+
+  var bucket = this._storage.get(index);
+
+  if (!bucket) {
+    return;
+  }
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+      return tuple[1];
+    }
+  }
 };
 
 
