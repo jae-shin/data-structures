1a7ae56fe2cf963e500029fe8dee2f6a61d36a88
jae-shin <jae5464@gmail.com>
2016-06-10 11:45:44 -0700

    Delete commented out implementation code

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,17 +11,6 @@ HashTable.prototype.insert = function(k, v) {
   if (!bucket) {
     this._storage.set(index, [[k, v]]);
   } else {
-    
-    /*// check if a tuple with same key exists, if so overwrite
-    for (var i = 0; i < bucket.length; i++) {
-      var tuple = bucket[i];
-      if (tuple[0] === k) {
-        tuple[1] = v;
-        return;
-      }
-    }*/
-
-    // TODO: refactor for loop with native findIndex()
     var bucketIndex = bucket.findIndex(function(tuple) {
       return tuple[0] === k;
     });
@@ -36,22 +25,12 @@ HashTable.prototype.insert = function(k, v) {
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // return this._storage.get(index)[k];
   var bucket = this._storage.get(index);
 
   if (!bucket) {
     return;
   }
 
-/*  for (var i = 0; i < bucket.length; i++) {
-    var tuple = bucket[i];
-    if (tuple[0] === k) {
-      return tuple[1];
-    }
-  }
-*/
-
-  // TODO: refactor for loop with native findIndex()
   var bucketIndex = bucket.findIndex(function(tuple) {
     return tuple[0] === k;
   });
@@ -63,25 +42,12 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //this._storage.set(index, undefined);
-  // this.insert(k, undefined);
-  //delete this._storage.get(index)[k];
-
   var bucket = this._storage.get(index);
 
   if (!bucket) {
     return;
   }
 
-  /*for (var i = 0; i < bucket.length; i++) {
-    var tuple = bucket[i];
-    if (tuple[0] === k) {
-      bucket.splice(i, 1);
-      return tuple[1];
-    }
-  }*/
-
-  // TODO: refactor for loop with native findIndex()
   var bucketIndex = bucket.findIndex(function(tuple) {
     return tuple[0] === k;
   });
