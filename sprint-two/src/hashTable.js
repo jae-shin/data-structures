079d8844e5b003c323755df27450875e0bc04206
jae-shin <jae5464@gmail.com>
2016-06-09 18:57:28 -0700

    Resolve collision for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,21 +7,24 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // if (this._storage.get(index)) {
-  //   index += 1;
-  // }
-  this._storage.set(index, v);
+  var obj = this._storage.get(index);
+  if (obj === undefined) {
+    obj = {};
+  }
+  obj[k] = v;
+  this._storage.set(index, obj);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
-  //var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
   //this._storage.set(index, undefined);
-  this.insert(k, undefined);
+  // this.insert(k, undefined);
+  delete this._storage.get(index)[k];
 };
 
 
