79c1b0386ca536d9bff5c691fa142facfea4d50b
Jae Shin <jae5464@gmail.com>
2016-06-11 09:02:59 -0700

    Add new file for implementation of advancedTree

---
+++ sprint-two/src/advancedTree.js
@@ -0,0 +1,36 @@
+var advancedTree = function(value) {
+  var newTree = {};
+  _.extend(newTree, treeMethods);
+  newTree.value = value;
+
+  // your code here
+  newTree.children = [];  // fix me
+
+  return newTree;
+};
+
+var treeMethods = {};
+
+treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
+};
+
+treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  // children
+  return _.reduce(this.children, function (acc, child) {
+    //if (child.contains(target)) { return true; }
+    if (acc) { return true; }
+    return child.contains(target);
+  }, false);
+
+};
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
