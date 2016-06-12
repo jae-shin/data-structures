7c48e581bf8bbc70509cf94e6181af8abf413d7b
Jae Shin <jae5464@gmail.com>
2016-06-12 10:14:27 -0700

    Refactor tests for parent property

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,63 +41,49 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
-});
+  describe('advanced tree', function() {
+    var tree;
 
-describe('advancedTree', function() {
-  var advancedTree;
+    beforeEach(function() {
+      tree = Tree();
+    });
 
-  beforeEach(function() {
-    advancedTree = AdvancedTree();
+    it('should have methods named "addChild" and "contains" and "removeFromParent", and a property named "value" and "parent"', function() {
+      expect(tree.addChild).to.be.a('function');
+      expect(tree.contains).to.be.a('function');
+      expect(tree.removeFromParent).to.be.a('function');
+      expect(tree.hasOwnProperty('value')).to.equal(true);
+      expect(tree.hasOwnProperty('parent')).to.equal(true);
+    });
+
+    it('should correctly refer to parent', function() {
+      tree.value = 10;
+      expect(tree.parent).to.equal(null);
+      tree.addChild(20);
+      expect(tree.children[0].parent.value).to.equal(10);
+      tree.addChild(21);
+      expect(tree.children[1].parent.value).to.equal(10);
+      tree.children[0].addChild(30);
+      expect(tree.children[0].children[0].parent.value).to.equal(20);
+      tree.children[1].addChild(31);
+      expect(tree.children[1].children[0].parent.value).to.equal(21);
+    });
+
+    it('should correctly remove from parent', function() {
+      tree.addChild(5);
+      tree.addChild(6);
+      tree.children[0].addChild(7);
+      tree.children[0].addChild(9);
+      tree.children[1].addChild(8);
+      expect(tree.removeFromParent()).to.equal(undefined);
+      expect(tree.parent).to.equal(null);
+      expect(tree.contains(9)).to.equal(true);
+      var leftChild = tree.children[0];
+      expect(leftChild.removeFromParent()).to.equal(tree);
+      expect(leftChild.parent).to.equal(null);
+      expect(tree.children[0].value).to.equal(6);
+      expect(tree.contains(7)).to.equal(false);
+    });
   });
 
-  it('should have methods named "addChild" and "contains" and "removeFromParent", and a property named "value" and "parent"', function() {
-    expect(advancedTree.addChild).to.be.a('function');
-    expect(advancedTree.contains).to.be.a('function');
-    expect(advancedTree.removeFromParent).to.be.a('function');
-    expect(advancedTree.hasOwnProperty('value')).to.equal(true);
-    expect(advancedTree.hasOwnProperty('parent')).to.equal(true);
-  });
-
-  it('should add children to the advancedTree', function() {
-    advancedTree.addChild(5);
-    expect(advancedTree.children[0].value).to.equal(5);
-  });
-
-  it('should return true for a value that the advancedTree contains', function() {
-    advancedTree.addChild(5);
-    expect(advancedTree.contains(5)).to.equal(true);
-  });
-
-  it('should return false for a value that was not added', function() {
-    advancedTree.addChild(5);
-    expect(advancedTree.contains(6)).to.equal(false);
-  });
-
-  it('should be able to add children to a advancedTree\'s child', function() {
-    advancedTree.addChild(5);
-    advancedTree.children[0].addChild(6);
-    expect(advancedTree.children[0].children[0].value).to.equal(6);
-  });
-
-  it('should correctly detect nested children', function() {
-    advancedTree.addChild(5);
-    advancedTree.addChild(6);
-    advancedTree.children[0].addChild(7);
-    advancedTree.children[1].addChild(8);
-    expect(advancedTree.contains(7)).to.equal(true);
-    expect(advancedTree.contains(8)).to.equal(true);
-  });
-
-  it('should correctly refer to parent', function() {
-    advancedTree.value = 10;
-    expect(advancedTree.parent).to.equal(null);
-    advancedTree.addChild(20);
-    expect(advancedTree.children[0].parent.value).to.equal(10);
-    advancedTree.addChild(21);
-    expect(advancedTree.children[1].parent.value).to.equal(10);
-    advancedTree.children[0].addChild(30);
-    expect(advancedTree.children[0].children[0].parent.value).to.equal(20);
-    advancedTree.children[1].addChild(31);
-    expect(advancedTree.children[1].children[0].parent.value).to.equal(21);
-  });
 });
